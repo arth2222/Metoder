72f2a4bbdc6e0c9dae179a4ac3e57cf2d55fad90
Per A. Bergersen <pab2222@hotmail.com>
2024-09-11 13:14:32 +0200

    Add project files.

---
+++ ConsoleApp1Slett9999/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Slett9999
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "123 ";
+            string trimmet=test.Trim();
+        }
+
+        public static void SkrivHei()
+        {
+
+        }
+
+        public static string ReturnerHei()
+        {
+            string bolle = "Hei";
+            return bolle;
+        }
+
+        public static void SkrivNoe(string tekst)
+        {
+            Console.WriteLine(tekst);
+        }
+
+
+        // Function to remove a character at the specified index
+        public static string remove_char(string str, int n)
+        {
+            return str.Remove(0, 5); // Using Remove method to eliminate the character at index n
+        }
+
+
+        public static int Add(int tall1, int tall2)
+        {
+            return tall1 + tall2;
+        }
+    }
+}
